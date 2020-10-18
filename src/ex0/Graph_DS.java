a204c4864d0fb306795e56e2c1c726c24b0ec64b
Ido Guzi <ido127@hotmail.com>
2020-10-18 19:38:39 +0300

    unoptimized Graph_DS and NodeData

--- src/ex0/Graph_DS.java
+++ src/ex0/Graph_DS.java
@@ -1,61 +1,102 @@
 package ex0;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Graph_DS implements graph{
+    private int nodeCount, edgeCount, modeCount;
+    private ArrayList<node_data> nodes;
+
+    public Graph_DS(){
+        nodeCount=0;
+        edgeCount=0;
+        modeCount=0;
+        nodes = new ArrayList<>();
+    }
+
+
 
     @Override
     public node_data getNode(int key) {
+        Iterator<node_data> it = nodes.iterator();
+        while (it.hasNext()){
+            node_data n = it.next();
+            if (n.getKey()==key) return n;
+        }
         return null;
     }
 
     @Override
     public boolean hasEdge(int node1, int node2) {
+        node_data n1 = getNode(node1);
+        if (n1.hasNi(node2)) return true;
         return false;
     }
 
     @Override
     public void addNode(node_data n) {
-
+        nodes.add(n);
+        nodeCount++;
+        modeCount++;
     }
 
     @Override
     public void connect(int node1, int node2) {
-
+        node_data n1 = getNode(node1), n2 = getNode(node2);
+        if (n1.hasNi(node2) && n2.hasNi(node1)) return;
+        n1.addNi(n2);
+        n2.addNi(n1);
+        edgeCount++;
+        modeCount++;
     }
 
     @Override
     public Collection<node_data> getV() {
-        return null;
+        return nodes;
     }
 
     @Override
     public Collection<node_data> getV(int node_id) {
-        return null;
+        node_data n = getNode(node_id);
+        return n.getNi();
     }
 
     @Override
     public node_data removeNode(int key) {
-        return null;
+        node_data n = getNode(key);
+        if (n==null) return null;
+        Collection<node_data> edges = n.getNi();
+        Iterator<node_data> it = edges.iterator();
+        while (it.hasNext()){
+            node_data t = it.next();
+            t.removeNode(n);
+        }
+        modeCount++;
+        return n;
     }
 
     @Override
     public void removeEdge(int node1, int node2) {
-
+        node_data n1 = getNode(node1), n2 = getNode(node2);
+        n1.removeNode(n2);
+        n2.removeNode(n1);
+        edgeCount--;
+        modeCount++;
     }
 
     @Override
     public int nodeSize() {
-        return 0;
+        return nodeCount;
     }
 
     @Override
     public int edgeSize() {
-        return 0;
+        return edgeCount;
     }
 
     @Override
     public int getMC() {
-        return 0;
+        return modeCount;
     }
 }
--- src/ex0/NodeData.java
+++ src/ex0/NodeData.java
@@ -1,50 +1,67 @@
 package ex0;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class NodeData implements node_data{
+    private int key, tag;
+    private ArrayList<node_data> neighbor;
+    private String info;
+    private static int keyCount=1;
+
+
+    public NodeData(){
+        key=keyCount++;
+    }
+
     @Override
     public int getKey() {
-        return 0;
+        return key;
     }
 
     @Override
     public Collection<node_data> getNi() {
-        return null;
+        return neighbor;
     }
 
     @Override
     public boolean hasNi(int key) {
+        Iterator<node_data> it = neighbor.iterator();
+        while(it.hasNext()){
+            node_data n = it.next();
+            if (n.getKey()==key) return true;
+        }
         return false;
     }
 
     @Override
     public void addNi(node_data t) {
-
+        neighbor.add(t);
     }
 
     @Override
     public void removeNode(node_data node) {
-
+        neighbor.remove(node);
     }
 
     @Override
     public String getInfo() {
-        return null;
+        return info;
     }
 
     @Override
     public void setInfo(String s) {
-
+        info=s;
     }
 
     @Override
     public int getTag() {
-        return 0;
+        return tag;
     }
 
     @Override
     public void setTag(int t) {
-
+        tag=t;
     }
 }
